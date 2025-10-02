1a984e130323d2d8ffe5d507de9d91fba8a7d23c
Amila Samarasinghe <90022013+amilax@users.noreply.github.com>
2025-10-02 21:57:55 +0530

    change page order

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
+current_status_page = st.Page(
+    page= "pages/current_status.py",
+    title= "Current Attendance",
+    default = True
+)
+
 attendence_page = st.Page(
     page= "pages/attendance.py",
     title= "Mark Attendance",
-    default = True
 )
 
 add_new_page = st.Page(
@@ -11,17 +16,12 @@ add_new_page = st.Page(
     title= "Add New",
 )
 
-current_status_page = st.Page(
-    page= "pages/current_status.py",
-    title= "Current Attendance",
-)
-
 teams_page = st.Page(
     page= "pages/teams.py",
     title= "Teams Details",
 )
 
-pg = st.navigation(pages = [attendence_page,add_new_page,current_status_page,teams_page])
+pg = st.navigation(pages = [current_status_page,attendence_page,add_new_page,teams_page])
 
 st.logo("logo.png",size="large")
 
